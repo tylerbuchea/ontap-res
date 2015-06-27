79124ecc710e3a4fe06b8522e2613ce68eb307b0
Tyler Buchea <tyler@buchea.com>
2015-06-27 12:37:34 -0700

    First commit.

---
+++ mailer.php
@@ -0,0 +1,72 @@
+<?php
+if(empty($_POST) || !isset($_POST)) {
+
+  ajaxResponse('error', 'Post cannot be empty.');
+
+} else {
+
+  $postData = $_POST;
+  $dataString = implode($postData,",")
+  $mailgun = sendMailgun($postData);
+
+  if($mailgun) {
+    ajaxResponse('success', 'Great success.', $postData, $mailgun);
+  } else {
+    ajaxResponse('error', 'Mailgun did not connect properly.', $postData, $mailgun);
+  }
+
+}
+
+function ajaxResponse($status, $message, $data = NULL, $mg = NULL) {
+  $response = array (
+    'status' => $status,
+    'message' => $message,
+    'data' => $data,
+    'mailgun' => $mg
+    );
+  $output = json_encode($response);
+  exit($output);
+}
+
+function sendMailgun($data) {
+
+  $api_key = 'INSERT_API_KEY_HERE';
+  $api_domain = 'INSERT_DOMAIN_HERE';
+  $send_to = 'YOUR_EMAIL';
+
+  $name = $data['name'];
+  $email = $data['email'];
+  $content = $data['message'];
+
+  $messageBody = "Contact: $name ($email)\n\nMessage: $content";
+
+  $config = array();
+  $config['api_key'] = $api_key;
+  $config['api_url'] = 'https://api.mailgun.net/v2/'.$api_domain.'/messages';
+
+  $message = array();
+  $message['from'] = $email;
+  $message['to'] = $send_to;
+  $message['h:Reply-To'] = $email;
+  $message['subject'] = $data['subject'];
+  $message['text'] = $messageBody;
+
+  $curl = curl_init();
+
+  curl_setopt($curl, CURLOPT_URL, $config['api_url']);
+  curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  curl_setopt($curl, CURLOPT_USERPWD, "api:{$config['api_key']}");
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS,$message);
+
+  $result = curl_exec($curl);
+
+  curl_close($curl);
+  return $result;
+
+}
+?>
